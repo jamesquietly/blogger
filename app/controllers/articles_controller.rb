725a22f4f4f401b4814d5d967352c17a19f7bea8
jamesquietly <jamesquietly@gmail.com>
2017-08-30 01:46:23 -0700

    fixed user check for delete and edit

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -21,7 +21,7 @@ class ArticlesController < ApplicationController
 
   def create
     @article = Article.new(article_params)
-    @article.author_name = current_user.username
+    @article.author_name = current_user.id.to_s
     @article.save
 
     flash.notice = "Article '#{@article.title}' was created!"
